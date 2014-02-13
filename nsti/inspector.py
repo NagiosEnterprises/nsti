d5cc52b2a7646667032d41076970902065a39325
lgroschen <lgroschen@nagios.com>
2014-02-13 16:25:40 -0600

    Added inspector.py and inspector.html, updated __init__.py and traplist.html (inspector not working yet)

---
+++ nsti/inspector.py
@@ -0,0 +1,22 @@
+from nsti import app
+import database as db
+
+from flask import render_template, session, request, abort, Response
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+@app.route('/inspector')
+def inspector():
+	session['TRUNCATE'] = app.config.get('TRUNCATE')
+	table = request.args.get('traptype') or session.get('traptype') or 'Snmptt'
+	c_tablename = table.capitalize()
+    
+    if c_tablename in ['Snmptt', 'SnmpttArchive', 'SnmpttUnknown']:
+        session['tablename'] = c_tablename
+    else:
+        abort(400, 'Bad Request. Inspector could not find what you specified, if you can believe that. Got: %s, expected Snmptt, SnmpttArchive or SnmpttUnknown.' % table)
+    
+	return render_template('trapview/traplist.html')

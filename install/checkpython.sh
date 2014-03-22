fae1b7945de8f7838ed3751208a9c81ba56d1b88
Nicholas Scott <scot0357@gmail.com>
2014-03-22 07:29:54 +0000

    Moving away from actually compiling Python as thats going to lead to tremendous headaches.

--- install/checkpython.sh
+++ install/checkpython.sh
@@ -1,51 +1,28 @@
 #!/bin/sh
 
-PYTHON_URL='https://www.python.org/ftp/python/2.7/Python-2.7.tar.bz2'
-PYTHON_VER='Python-2.7'
-PYTHON_CFG='--enable-shared'
-
-install_python () {
-    (
-        cd /tmp
-
-        if [ ! -f "${PYTHON_VER}.tar.bz2" ];
-        then
-            wget "$PYTHON_URL" --no-check-certificate
-        fi
-
-        tar xf "${PYTHON_VER}.tar.bz2"
-        cd "$PYTHON_VER"
-        ./configure $PYTHON_CFG
-        make && make altinstall
-
-        cd ..
-        rm "${PYTHON_VER}.tar.bz2" -f
-    )
-}
+PIP_TAR='https://pypi.python.org/packages/source/p/pip/pip-1.1.tar.gz'
 
 install_python_pip () {
     (
         cd /tmp
 
-        if [ ! -f get-pip.py ];
+        if [ ! -f pip-1.1.tar.gz ];
         then
-            wget 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py' --no-check-certificate
+            wget "${PIP_TAR}" --no-check-certificate
         fi
+        
+        tar xf pip-1.1.tar.gz
+        cd pip-1.1
+        python setup.py install
 
-        python2.7 get-pip.py
-
-        rm get-pip.py -f
+        cd ..
+        rm -rf pip-1.1.tar.gz
+        rm -rf pip-1.1
     )
 }
 
-echo "Checking Python version..."
-if ! is_installed python2.7;
-then
-    install_python
-fi
-
 echo "Checking if pip is installed..."
-if ! is_installed pip2.7;
+if ! is_installed pip;
 then
     install_python_pip
 fi

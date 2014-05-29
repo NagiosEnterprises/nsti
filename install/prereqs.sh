8d99c599af975b9f52f7e14a8c6580b9f0bbea40
lgroschen <lgroschen@nagios.com>
2014-05-29 16:53:13 -0500

    rework install script

--- install/prereqs.sh
+++ install/prereqs.sh
@@ -2,10 +2,14 @@
 
 PREREQS='mysql httpd gcc wget make tar'
 
+#install prereqs- only supporting yum packager FOR NOW
+yum install -y mysql httpd gcc wget make tar mod_wsgi
+
 echo "Checking installer prereqs..."
 echo "-----------------------------"
 if ! is_installed "$PREREQS";
 then
+
     echo "Baseline prereqs are not installed. You must have these installed:"
     for prereq in "$PREREQS";
     do
@@ -14,39 +18,41 @@ then
     echo "Cannot continue install until all of these prereqs are met."
     exit 1
 fi
-echo "----------------------------"
+echo "-----------------------"
 echo "INSTALLER PREREQS MET"
-echo "----------------------------"
+echo "-----------------------"
 echo ""
 
 echo "Making sure WSGI is installed in Apache..."
 echo "------------------------------------------"
 if ! httpd -M 2> /dev/null | grep wsgi;
 then
-    echo "mod_wsgi is not install in Apache. Please use your package manager to install it."
+    echo "mod_wsgi is not installed in Apache. Please use your package manager to install it."
     exit 1
 fi
-echo "----------------------------"
+echo "---------------------"
 echo "MOD_WSGI INSTALLED"
-echo "----------------------------"
+echo "---------------------"
 echo ""
+
+# Add nagios user
+add_user nagios
+add_group nagios
+add_group nagcmd
+
+# Add nagios user to nagios group
+add_to_groups nagios nagios nagcmd
+add_to_groups apache nagios nagcmd
 
 echo "Creating users and groups..."
 echo "----------------------------"
 if ! cat /etc/passwd | grep nagios;
 then
-
-    # Add nagios user
-    add_user nagios
-    add_group nagios
-    add_group nagcmd
-	
-    # Add nagios user to nagios group
-    add_to_groups nagios nagios nagcmd
-    add_to_groups apache nagios nagcmd
-
+    echo 'No user `nagios`. Add a user for nagios and make sure it is in the nagcmd group.'
+    echo 'Apache must also be in nagcmd.'
+    exit 1
 fi
-echo "---------------------------"
-echo "USERS/GROUPS CREATED"
-echo "---------------------------"
+echo "------------------"
+echo "USERS/GROUPS OK"
+echo "------------------"
 echo ""

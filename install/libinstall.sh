089af9c4de35c198b499aff8c134756cdaa92a1e
Jake Omann <jomann@nagios.com>
2014-05-29 15:40:24 -0500

    Add user/group functions to libinstall

--- install/libinstall.sh
+++ install/libinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Checks if NSTI has been installed
 is_installed () {
     local binary="$1"
     for file in $@;
@@ -14,5 +15,45 @@ is_installed () {
 }
 
 BASEPATH=$(dirname `readlink -f $0`)
+
+# Adds specified user if it doesn't exist already
+add_user() {
+	local user
+	user="$1"
+
+	if ! grep -q "^$user:" /etc/passwd; then
+		case "$dist" in
+			el5 )
+				useradd -N "$user"
+				;;
+			el* )
+				useradd -n "$user"
+				;;
+			* )
+				useradd "$user"
+		esac
+	fi
+}
+
+# Adds specified group if it doesn't exist already
+add_group() {
+	local group
+	group="$1"
+
+	if ! grep -q "^$group:" /etc/group; then
+		groupadd "$group"
+	fi
+}
+
+# Adds user to the specified groups
+add_to_groups() {
+	local user
+	user="$1"
+
+	shift
+	for group; do
+		usermod -a -G "$group" "$user"
+	done
+}
 
 . "$BASEPATH/nsti/etc/nsti.py"

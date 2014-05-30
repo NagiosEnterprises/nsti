503eef67eaf9a06a00b977d274de7f191a2d436b
lgroschen <lgroschen@nagios.com>
2014-05-30 11:34:47 -0500

    remove host from mysql check command so it doesnt fail

--- install/database.sh
+++ install/database.sh
@@ -4,7 +4,7 @@ DB_SCHEMA="${BASEPATH}/nsti/dist/nsti.sql"
 
 mysqladmin -s -u${DB_USER} -p${DB_PASS} create ${DB_NAME}
 
-if ! mysql -u${DB_USER} -p${DB_PASS} -h${DB_HOST} ${DB_NAME} < $DB_SCHEMA;
+if ! mysql -u${DB_USER} -p${DB_PASS} ${DB_NAME} < $DB_SCHEMA;
 then
     echo "Unable to add database schema. Does the ${DB_NAME} exist?"
     exit 1

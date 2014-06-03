996e3f05a27a7b8e68185591a7ca0fac3887ef8f
Nicholas Scott <scot0357@gmail.com>
2014-06-03 15:42:36 -0500

    Changing the default user from root to snmpttuser, for our own safety

--- nsti/etc/nsti.py
+++ nsti/etc/nsti.py
@@ -4,8 +4,8 @@ DB_TYPE='mysql'
 DB_HOST='127.0.0.1'
 DB_PORT='3306'
 DB_NAME='snmptt'
-DB_USER='root'
-DB_PASS='nsti'
+DB_USER='snmpttuser'
+DB_PASS='password'
 
 #~ Page Display Options
 TRUNCATE=100

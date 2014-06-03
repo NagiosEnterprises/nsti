2a466a4ee922bf2b4e5d906e4dbaf45461e581f6
Nicholas Scott <scot0357@gmail.com>
2014-06-03 15:30:10 -0500

    Added the commands to edit the:
    mode = daemon
    dns_enable = 1
    mysql_dbi_enable = 1
    
    Using a sed inplace replacement.

---
+++ install/snmptt.sh
@@ -0,0 +1,5 @@
+SNMPTTINI="/etc/snmp/snmptt.ini"
+
+sed -i'bkp' 's/^mode[ \t]*=[ \t]*standalone/mode = daemon/g' "$SNMPTTINI"
+sed -i'bkp' 's/^dns_enable[ \t]*=[ \t]*0/dns_enable = 1/g' "$SNMPTTINI"
+sed -i'bkp' 's/^mysql_dbi_enable[ \t]*=[ \t]*0/mysql_dbi_enable = 1/g' "$SNMPTTINI"

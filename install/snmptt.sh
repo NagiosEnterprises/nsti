2ff7e13e1b79867bcb9646dd25a267f2047598e6
lgroschen <lgroschen@nagios.com>
2015-02-11 12:48:01 -0600

    adding snmptrapd config for authentication and traphandling

--- install/snmptt.sh
+++ install/snmptt.sh
@@ -1,6 +1,10 @@
 SNMPTTINI="/etc/snmp/snmptt.ini"
+SNMPTRAPD="/etc/snmp/snmptrapd.conf"
 
 sed -i'.bkp' 's/^mode[ \t]*=[ \t]*standalone/mode = daemon/g' "$SNMPTTINI"
 sed -i'.bkp' 's/^dns_enable[ \t]*=[ \t]*0/dns_enable = 1/g' "$SNMPTTINI"
 sed -i'.bkp' 's/^mysql_dbi_enable[ \t]*=[ \t]*0/mysql_dbi_enable = 1/g' "$SNMPTTINI"
 sed -i'.bkp' 's/^net_snmp_perl_enable[ \t]*=[ \t]*0/net_snmp_perl_enable = 1/g' "$SNMPTTINI"
+
+# set snmptrapd authCommunity and traphandle
+sed -i'.bkp' '$a\ #disableAuthorization yes\authCommunity    log,execute,net    public\traphandle default /usr/sbin/snmptthandler\' "$SNMPTRAPD"

9539455c65b755fa8542a0bcd6d8944fd1857d22
lgroschen <lgroschen@nagios.com>
2015-02-11 15:14:38 -0600

    test

--- install/apacheconfig.sh
+++ install/apacheconfig.sh
@@ -5,7 +5,7 @@ cp ${BASEPATH}/nsti/dist/apache.conf /etc/httpd/conf.d/nsti.conf
 # Applying patch for Apache 2.4 configs in CentOS 7
 # without this patch, access is denied to all pages in NSTI
 if [ $dist -eq 'el7' ]; then
-    echo "Applying patches to Apache configs for Apache 2.4.x syntax..."
+    echo "Applying patches to Apache configs for Apache 2.4.x syntax.. "
     sed -i '/<\/Directory>/c\   Require all granted\n</Directory>' /etc/httpd/conf.d/$f.conf
 fi
 
